6807af33993bb9c4f315322b65ee4b9532746919
aaaasmile <testhaus@gmx.at>
2025-01-03 18:09:43 +0100

    some saml experiments

--- go.mod
+++ go.mod
@@ -4,18 +4,27 @@ go 1.21.1
 
 require (
 	github.com/BurntSushi/toml v1.4.0
+	github.com/crewjam/saml v0.4.14
 	github.com/fsnotify/fsnotify v1.8.0
 	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
 	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0
 	github.com/mattn/go-sqlite3 v1.14.24
+	github.com/microcosm-cc/bluemonday v1.0.27
+	github.com/russross/blackfriday/v2 v2.1.0
 	golang.org/x/image v0.23.0
 )
 
 require (
 	github.com/aymerick/douceur v0.2.0 // indirect
+	github.com/beevik/etree v1.1.0 // indirect
+	github.com/crewjam/httperr v0.2.0 // indirect
+	github.com/golang-jwt/jwt/v4 v4.4.3 // indirect
 	github.com/gorilla/css v1.0.1 // indirect
-	github.com/microcosm-cc/bluemonday v1.0.27 // indirect
-	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/jonboulle/clockwork v0.2.2 // indirect
+	github.com/mattermost/xml-roundtrip-validator v0.1.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/russellhaering/goxmldsig v1.3.0 // indirect
+	golang.org/x/crypto v0.24.0 // indirect
 	golang.org/x/net v0.26.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
 )
--- web/web.go
+++ web/web.go
@@ -1,19 +1,26 @@
 package web
 
 import (
+	"context"
 	"corsa-blog/conf"
 	"corsa-blog/idl"
 	"corsa-blog/mail"
 	"corsa-blog/telegram"
 	"corsa-blog/util"
 	"corsa-blog/web/app"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"os/signal"
 	"strings"
 	"time"
+
+	"github.com/crewjam/saml/samlsp"
 )
 
 type ObserveComments struct {
@@ -34,6 +41,12 @@ func RunService(configfile string, simulate bool) error {
 	log.Println("Server started with URL ", serverurl)
 	log.Println("Try this url for Dashboard: ", dashboardServURL)
 
+	// Dashboard app
+	//http.HandleFunc(conf.Current.RootURLPattern, app.APiHandler)
+	if err := prepareSaml(dashboardServURL); err != nil {
+		return err
+	}
+
 	staticPathHnd := conf.Current.RootURLPattern + "static/"
 	staticDirSrv := http.Dir(util.GetFullPath("static"))
 	log.Println("static handler for dashboard on ", staticPathHnd, staticDirSrv)
@@ -43,9 +56,6 @@ func RunService(configfile string, simulate bool) error {
 	staticBlogDirSrv := http.Dir(util.GetFullPath(fmt.Sprintf("static/%s", conf.Current.StaticBlogDir)))
 	log.Println("static blog dir", staticBlogDirSrv)
 	http.Handle("/", http.StripPrefix("/", http.FileServer(staticBlogDirSrv)))
-
-	// Dashboard app
-	http.HandleFunc(conf.Current.RootURLPattern, app.APiHandler)
 
 	chShutdown := make(chan struct{}, 1)
 	go func(chs chan struct{}) {
@@ -90,6 +100,66 @@ loop:
 	}
 
 	log.Println("Bye, service")
+	return nil
+}
+
+func prepareSaml(dashboardUri string) error {
+	log.Println("prepare  saml on ", dashboardUri)
+	keyPair, err := tls.LoadX509KeyPair("./cert/igorrun.cert", "./cert/igorrun.key")
+	if err != nil {
+		log.Println("ERROR 509 key pair")
+		return err
+	}
+	keyPair.Leaf, err = x509.ParseCertificate(keyPair.Certificate[0])
+	if err != nil {
+		log.Println("ERROR certificate")
+		return err
+	}
+	myReader := strings.NewReader(`<EntityDescriptor xmlns="urn:oasis:names:tc:SAML:2.0:metadata" validUntil="2015-12-03T01:57:09Z" entityID="http://localhost:5572/blog-admin/saml/metadata"><SPSSODescriptor xmlns="urn:oasis:names:tc:SAML:2.0:metadata" validUntil="0001-01-01T00:00:00Z" protocolSupportEnumeration="urn:oasis:names:tc:SAML:2.0:protocol" AuthnRequestsSigned="false" WantAssertionsSigned="true"><KeyDescriptor use="signing"><KeyInfo xmlns="http://www.w3.org/2000/09/xmldsig#"><X509Data xmlns="http://www.w3.org/2000/09/xmldsig#"><X509Certificate xmlns="http://www.w3.org/2000/09/xmldsig#">MIIB7zCCAVgCCQDFzbKIp7b3MTANBgkqhkiG9w0BAQUFADA8MQswCQYDVQQGEwJVUzELMAkGA1UECAwCR0ExDDAKBgNVBAoMA2ZvbzESMBAGA1UEAwwJbG9jYWxob3N0MB4XDTEzMTAwMjAwMDg1MVoXDTE0MTAwMjAwMDg1MVowPDELMAkGA1UEBhMCVVMxCzAJBgNVBAgMAkdBMQwwCgYDVQQKDANmb28xEjAQBgNVBAMMCWxvY2FsaG9zdDCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEA1PMHYmhZj308kWLhZVT4vOulqx/9ibm5B86fPWwUKKQ2i12MYtz07tzukPymisTDhQaqyJ8Kqb/6JjhmeMnEOdTvSPmHO8m1ZVveJU6NoKRn/mP/BD7FW52WhbrUXLSeHVSKfWkNk6S4hk9MV9TswTvyRIKvRsw0X/gfnqkroJcCAwEAATANBgkqhkiG9w0BAQUFAAOBgQCMMlIO+GNcGekevKgkakpMdAqJfs24maGb90DvTLbRZRD7Xvn1MnVBBS9hzlXiFLYOInXACMW5gcoRFfeTQLSouMM8o57h0uKjfTmuoWHLQLi6hnF+cvCsEFiJZ4AbF+DgmO6TarJ8O05t8zvnOwJlNCASPZRH/JmF8tX0hoHuAQ==</X509Certificate></X509Data></KeyInfo></KeyDescriptor><KeyDescriptor use="encryption"><KeyInfo xmlns="http://www.w3.org/2000/09/xmldsig#"><X509Data xmlns="http://www.w3.org/2000/09/xmldsig#"><X509Certificate xmlns="http://www.w3.org/2000/09/xmldsig#">MIIB7zCCAVgCCQDFzbKIp7b3MTANBgkqhkiG9w0BAQUFADA8MQswCQYDVQQGEwJVUzELMAkGA1UECAwCR0ExDDAKBgNVBAoMA2ZvbzESMBAGA1UEAwwJbG9jYWxob3N0MB4XDTEzMTAwMjAwMDg1MVoXDTE0MTAwMjAwMDg1MVowPDELMAkGA1UEBhMCVVMxCzAJBgNVBAgMAkdBMQwwCgYDVQQKDANmb28xEjAQBgNVBAMMCWxvY2FsaG9zdDCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEA1PMHYmhZj308kWLhZVT4vOulqx/9ibm5B86fPWwUKKQ2i12MYtz07tzukPymisTDhQaqyJ8Kqb/6JjhmeMnEOdTvSPmHO8m1ZVveJU6NoKRn/mP/BD7FW52WhbrUXLSeHVSKfWkNk6S4hk9MV9TswTvyRIKvRsw0X/gfnqkroJcCAwEAATANBgkqhkiG9w0BAQUFAAOBgQCMMlIO+GNcGekevKgkakpMdAqJfs24maGb90DvTLbRZRD7Xvn1MnVBBS9hzlXiFLYOInXACMW5gcoRFfeTQLSouMM8o57h0uKjfTmuoWHLQLi6hnF+cvCsEFiJZ4AbF+DgmO6TarJ8O05t8zvnOwJlNCASPZRH/JmF8tX0hoHuAQ==</X509Certificate></X509Data></KeyInfo><EncryptionMethod Algorithm="http://www.w3.org/2001/04/xmlenc#aes128-cbc"></EncryptionMethod><EncryptionMethod Algorithm="http://www.w3.org/2001/04/xmlenc#aes192-cbc"></EncryptionMethod><EncryptionMethod Algorithm="http://www.w3.org/2001/04/xmlenc#aes256-cbc"></EncryptionMethod><EncryptionMethod Algorithm="http://www.w3.org/2001/04/xmlenc#rsa-oaep-mgf1p"></EncryptionMethod></KeyDescriptor><AssertionConsumerService Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST" Location="http://localhost:5572/blog-admin/sam/acs" index="1"></AssertionConsumerService></SPSSODescriptor></EntityDescriptor>`)
+	req, err := http.NewRequest("PUT", "http://localhost:8000/services/sp",
+		myReader)
+	if err != nil {
+		log.Println("ERROR on PUT service")
+		return err
+	}
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Println("ERROR on PUT service")
+		return err
+	} else {
+		fmt.Println("*** res", res)
+	}
+
+	idpMetadataURL, err := url.Parse("http://localhost:8000/metadata")
+	if err != nil {
+		log.Println("ERROR url parse  samlidp")
+		return err
+	}
+	idpMetadata, err := samlsp.FetchMetadata(context.Background(), http.DefaultClient,
+		*idpMetadataURL)
+	if err != nil {
+		log.Println("ERROR fetch metadata")
+		return err
+	}
+
+	rootURL, err := url.Parse(dashboardUri)
+	if err != nil {
+		log.Println("ERROR url parse dashboard")
+		return err
+	}
+
+	samlSP, _ := samlsp.New(samlsp.Options{
+		URL:         *rootURL,
+		Key:         keyPair.PrivateKey.(*rsa.PrivateKey),
+		Certificate: keyPair.Leaf,
+		IDPMetadata: idpMetadata,
+	})
+	log.Println("saml setup ok")
+	appApi := http.HandlerFunc(app.APiHandler)
+	http.Handle(conf.Current.RootURLPattern, samlSP.RequireAccount(appApi))
+	http.Handle("/saml/", samlSP)
+
+	log.Println("saml handler setup completed")
 	return nil
 }
 
