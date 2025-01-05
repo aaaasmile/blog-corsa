dc66089ef429c540369e8a772cba88281d4d0155
aaaasmile <testhaus@gmx.at>
2025-01-05 16:35:48 +0100

    more link tests

--- content/src/mhparser/lexer-script-tokenizer_test.go
+++ content/src/mhparser/lexer-script-tokenizer_test.go
@@ -118,7 +118,6 @@ il nuovo`
 		t.Errorf("expected two html lines, but %d", len(ll.ArrayValue))
 		return
 	}
-	//t.Error("stop!")
 }
 
 func TestParseHtmlLinkBlock(t *testing.T) {
@@ -165,5 +164,154 @@ id: 20241108-00
 	if !strings.Contains(secline, "<p>Tracker: <a href=\"https://wien-rundumadum-2024-130k.legendstracking.com/\" target=\"_blank\">https://wien-rundumadum-2024-130k.legendstracking.com/</a></p>") {
 		t.Errorf("expected  <a> in generated  html, but %s ", secline)
 	}
-	//t.Error("stop!")
+}
+
+func TestParseHtmlLinkBlockThreeLines(t *testing.T) {
+	str := `title: Un altro post entusiasmante
+datetime: 2024-12-23
+id: 20241108-00
+---
+<p>Pa</p>
+<p>Tracker: [link 'https://wien-rundumadum-2024-130k.legendstracking.com/']</p>
+<div>hello</div>`
+
+	lex := ScriptGrammar{
+		Debug:    true,
+		TemplDir: "../templates/htmlgen",
+	}
+	err := lex.ParseScript(str)
+	if err != nil {
+		t.Error("Error is: ", err)
+		return
+	}
+
+	err = lex.CheckNorm()
+	if err != nil {
+		t.Error("Error in parser norm ", err)
+		return
+	}
+	err = lex.EvaluateParams()
+	if err != nil {
+		t.Error("Error in evaluate ", err)
+		return
+	}
+	nrm := lex.Norm["main"]
+	lastFns := len(nrm.FnsList) - 1
+	stFns := nrm.FnsList[lastFns]
+	if len(stFns.Params) != 1 && !stFns.Params[0].IsArray {
+		t.Error("expected one array param with lines")
+		return
+	}
+	ll := &stFns.Params[0]
+	if len(ll.ArrayValue) != 3 {
+		t.Errorf("expected 3 html lines, but %d", len(ll.ArrayValue))
+		return
+	}
+	secline0 := ll.ArrayValue[0]
+	if !strings.Contains(secline0, "<p>Pa</p>") {
+		t.Errorf("expected <p>Pa</p> in generated  html, but %s ", secline0)
+	}
+	secline := ll.ArrayValue[1]
+	if !strings.Contains(secline, "<p>Tracker: <a href=\"https://wien-rundumadum-2024-130k.legendstracking.com/\" target=\"_blank\">https://wien-rundumadum-2024-130k.legendstracking.com/</a></p>") {
+		t.Errorf("expected  <a href> in generated  html, but %s ", secline)
+	}
+	secline = ll.ArrayValue[2]
+	if !strings.Contains(secline, "<div>hello</div>") {
+		t.Errorf("expected  <div>hello</div> in generated  html, but %s ", secline)
+	}
+}
+
+func TestParseHtmlLinkBlockOneLine(t *testing.T) {
+	str := `title: Un altro post entusiasmante
+datetime: 2024-12-23
+id: 20241108-00
+---
+[link 'https://wien-rundumadum-2024-130k.legendstracking.com/']`
+
+	lex := ScriptGrammar{
+		Debug:    true,
+		TemplDir: "../templates/htmlgen",
+	}
+	err := lex.ParseScript(str)
+	if err != nil {
+		t.Error("Error is: ", err)
+		return
+	}
+
+	err = lex.CheckNorm()
+	if err != nil {
+		t.Error("Error in parser norm ", err)
+		return
+	}
+	err = lex.EvaluateParams()
+	if err != nil {
+		t.Error("Error in evaluate ", err)
+		return
+	}
+	nrm := lex.Norm["main"]
+	lastFns := len(nrm.FnsList) - 1
+	stFns := nrm.FnsList[lastFns]
+	if len(stFns.Params) != 1 && !stFns.Params[0].IsArray {
+		t.Error("expected one array param with lines")
+		return
+	}
+	ll := &stFns.Params[0]
+	if len(ll.ArrayValue) != 1 {
+		t.Errorf("expected one html lines, but %d", len(ll.ArrayValue))
+		return
+	}
+	secline := ll.ArrayValue[0]
+	if !strings.Contains(secline, "<a href=\"https://wien-rundumadum-2024-130k.legendstracking.com/\" target=\"_blank\">https://wien-rundumadum-2024-130k.legendstracking.com/</a>") {
+		t.Errorf("expected  <a> in generated  html, but %s ", secline)
+	}
+}
+
+func TestParseHtmlLinkBlockTwoLines(t *testing.T) {
+	str := `title: Un altro post entusiasmante
+datetime: 2024-12-23
+id: 20241108-00
+---
+[link 'https://wien-rundumadum-2024-130k.legendstracking.com/']<p>
+hello</p>`
+
+	lex := ScriptGrammar{
+		Debug:    true,
+		TemplDir: "../templates/htmlgen",
+	}
+	err := lex.ParseScript(str)
+	if err != nil {
+		t.Error("Error is: ", err)
+		return
+	}
+
+	err = lex.CheckNorm()
+	if err != nil {
+		t.Error("Error in parser norm ", err)
+		return
+	}
+	err = lex.EvaluateParams()
+	if err != nil {
+		t.Error("Error in evaluate ", err)
+		return
+	}
+	nrm := lex.Norm["main"]
+	lastFns := len(nrm.FnsList) - 1
+	stFns := nrm.FnsList[lastFns]
+	if len(stFns.Params) != 1 && !stFns.Params[0].IsArray {
+		t.Error("expected one array param with lines")
+		return
+	}
+	ll := &stFns.Params[0]
+	if len(ll.ArrayValue) != 2 {
+		t.Errorf("expected 2 html lines, but %d", len(ll.ArrayValue))
+		return
+	}
+	secline := ll.ArrayValue[0]
+	if !strings.Contains(secline, "<a href=\"https://wien-rundumadum-2024-130k.legendstracking.com/\" target=\"_blank\">https://wien-rundumadum-2024-130k.legendstracking.com/</a><p>") {
+		t.Errorf("expected  <a> in generated  html, but %s ", secline)
+	}
+	secline = ll.ArrayValue[1]
+	if !strings.Contains(secline, "hello</p>") {
+		t.Errorf("expected  hello</p> in generated  html, but %s ", secline)
+	}
 }
