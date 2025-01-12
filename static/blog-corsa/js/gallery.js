66d74fcf9beb265b8eb119f2f3527e40537f3cd9
aaaasmile <testhaus@gmx.at>
2025-01-12 21:15:25 +0100

    load data with sort

--- static/blog-corsa/js/gallery.js
+++ static/blog-corsa/js/gallery.js
@@ -1,29 +1,44 @@
 
-export default {
-    _data: {},
-    _galleryImages: {},
-    loadData(callBack) {
-        console.log('load data')
-        fetch('photos.json', { cache: 'no-store' })
-            .then(response => response.json())
-            .then((images) => images.sort((a, b) => a.name.localeCompare(b.name)))
-            .then((data) => {
-                _galleryImages = data;
-                console.log('data are: ', data)
-                // this.albums = new Map();
-                // data.forEach(element => {
-                //     if (!this.albums.has(element.textMeta.Directory)) {
-                //         this.albums.set(element.textMeta.Directory, new Array());
-                //     }
-                //     this.albums.get(element.textMeta.Directory).push(element);
-                // });
-            }).then(() => callBack(this))
-            .catch(err => {
-                console.error('error on fetch: ', err)
-            });
-    },
-    displayImage(id) {
-        console.log('display image id ', id)
-    }
+export default () => {
+    let _dataImages = {}
+    let _mapImg = new Map()
+    let _idArray = []
+    let _currentImg = {}
 
+    function resetStrc() {
+        _dataImages = {}
+        _mapImg = new Map()
+        _idArray = []
+        _currentImg = {}
+    }
+    return {
+        loadData() {
+            console.log('load data')
+            resetStrc()
+            fetch('photos.json', { cache: 'no-store' })
+                .then(response => response.json())
+                .then((data) => {
+                    //console.log('data from fetch: ', data)
+                    _dataImages = data;
+                    _dataImages = data.images.sort((a, b) => a.id.localeCompare(b.id))
+                    let index = 0
+                    _dataImages.forEach(item => {
+                        _mapImg.set(item.id, { name: item.name, redux: item.redux, caption: item.caption, ix: index })
+                        _idArray.push(item.id)
+                        index += 1
+                    })
+                    //console.log('dataimages: ', _dataImages)
+                    //console.log('mapImg: ', _mapImg)
+                    console.log('img data for gallery ok')
+                })
+                .catch(err => {
+                    console.error('error on fetch: ', err)
+                });
+        },
+        displayImage(id) {
+            console.log('display image id ', id)
+            _currentImg = _mapImg.get(id)
+            //console.log('current image ', _currentImg)    
+        }
+    }
 }
