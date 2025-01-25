b494990534bbb0ddb2418d787c4f4ae25337c082
aaaasmile <testhaus@gmx.at>
2025-01-25 22:54:11 +0100

    changed post id

--- idl/cmtItem.go
+++ idl/cmtItem.go
@@ -1,6 +1,6 @@
 package idl
 
 func (ci *CmtItem) GetLocationFromPostId() string {
-	// Example of result "/posts/2024/11/08/24-11-08-ProssimaGara/" from PostId 20241108-00
+	// Example of result "/posts/2024/11/08/24-11-08-ProssimaGara/" from PostId 24-11-08-ProssimaGara-PS
 	return "/"
 }
--- idl/cmtNode.go
+++ idl/cmtNode.go
@@ -5,6 +5,8 @@ import "fmt"
 func (cn *CmtNode) GetTextNumComments() string {
 	if cn.NodeCount == 1 {
 		return "1 commento"
+	} else if cn.NodeCount == 0 {
+		return "Nessun commento"
 	}
 	return fmt.Sprintf("%d commenti", cn.NodeCount)
 }
