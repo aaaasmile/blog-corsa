aa685353ee399fa3c3d0210779c8a71cd13e6310
aaaasmile <testhaus@gmx.at>
2026-04-12 20:54:01 +0200

    improved login view

--- static/vue/js/vue/views/Login.vue
+++ static/vue/js/vue/views/Login.vue
@@ -3,14 +3,14 @@
     <v-card-title class="subheading grey--text">Login</v-card-title>
     <v-divider></v-divider>
     <v-container>
-      <v-row>
-        <v-text-field v-model="User" label="User"></v-text-field>
-      </v-row>
-      <v-row>
-        <v-text-field v-model="Password" type="password" label="Password"></v-text-field>
+      <v-row justify="center">
+        <v-col cols="12" sm="6" md="4">
+          <v-text-field v-model="User" label="User"></v-text-field>
+          <v-text-field v-model="Password" type="password" label="Password"></v-text-field>
+        </v-col>
       </v-row>
     </v-container>
-    <v-card-actions>
+    <v-card-actions class="justify-center pb-4">
       <v-btn color="primary" v-on:click="doLogin">Login</v-btn>
     </v-card-actions>
   </v-card>
