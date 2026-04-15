4b0ea3a51fe1f2be2f551d5cd1f66df5b991b11d
aaaasmile <testhaus@gmx.at>
2026-04-15 22:17:39 +0200

    added confirm

--- static/vue/js/vue/views/components/race-editor.js
+++ static/vue/js/vue/views/components/race-editor.js
@@ -22,8 +22,11 @@ export default {
         { text: 'Title', value: 'Title' },
         { text: 'Distance', value: 'Distance' },
         { text: 'Date', value: 'Date' },
+        { text: 'Actions', value: 'actions', sortable: false },
       ],
       races: [],
+      dialogDelete: false,
+      raceToDelete: null,
     }
   },
   created() {
@@ -44,6 +47,19 @@ export default {
     },
     onCancelRace() {
       this.showRaceItem = false
+    },
+    deleteRace(item) {
+      this.raceToDelete = item
+      this.dialogDelete = true
+    },
+    confirmDelete() {
+      const index = this.races.indexOf(this.raceToDelete)
+      if (index > -1) {
+        this.races.splice(index, 1)
+        console.log('Race deleted', this.raceToDelete)
+      }
+      this.dialogDelete = false
+      this.raceToDelete = null
     },
   },
   template: `
@@ -75,6 +91,9 @@ export default {
           nextIcon: 'mdi-plus',
         }"
       >
+        <template v-slot:item.actions="{ item }">
+          <v-icon small color="red" @click="deleteRace(item)">mdi-delete</v-icon>
+        </template>
       </v-data-table>
     </v-card>
     <RaceItem
@@ -83,6 +102,19 @@ export default {
       @save="onSaveRace"
       @cancel="onCancelRace"
     />
+    <v-dialog v-model="dialogDelete" persistent max-width="350">
+      <v-card>
+        <v-card-title class="headline">Confirm Delete</v-card-title>
+        <v-card-text>
+          Are you sure you want to delete this race?
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="green darken-1" text @click="confirmDelete">OK</v-btn>
+          <v-btn color="green darken-1" text @click="dialogDelete = false">Cancel</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-container>
 `
 }
